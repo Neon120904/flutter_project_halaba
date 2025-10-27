188400548040b386413002ce38e9da017767c6e1
github <wzeddrick6@gmail.com>
2025-10-27 12:34:06 +0800

    Halaba_midterm

--- facebook/lib/pages/notifications.dart
+++ facebook/lib/pages/notifications.dart
@@ -10,6 +10,50 @@ class NotificationsPage extends StatefulWidget {
 class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notifications'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+        ],
+      ),
+      body: Column(   
+        children: [ 
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof1.jpg"),
+            ),
+            title: Text("John Doe liked your post."),
+            subtitle: Text("2 hrs ago"),
+            trailing: Icon(Icons.more_horiz),
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof2.jpg"),
+            ),
+            title: Text("Jane Smith commented on your photo."),
+            subtitle: Text("3 hrs ago"),
+            trailing: Icon(Icons.more_horiz),
+         ), 
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof3.jpg"),
+            ),
+            title: Text("Alice Johnson started following you."),
+            subtitle: Text("5 hrs ago"),
+            trailing: Icon(Icons.more_horiz),
+          ), 
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/profile/prof4.jpg"),
+            ),
+            title: Text("Bob Brown shared your post."),
+            subtitle: Text("1 day ago"),
+            trailing: Icon(Icons.more_horiz),
+          ),
+        ],
+    ));
   }
 }
+
